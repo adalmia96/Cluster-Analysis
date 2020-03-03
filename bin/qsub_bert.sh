5a24a606be2d9525994447d3d2bd9061baecc267
suzyahyah <suzyahyah@gmail.com>
2020-03-03 12:55:09 -0500

    sanity check NPMI

---
+++ bin/qsub_bert.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python bert_encode.py
+
--- score.py
+++ score.py
@@ -10,13 +10,14 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import math
 
 import gensim
 
 import fasttext.util
 import fasttext
 
-NSEEDS = 5
+NSEEDS = 1
 
 def main():
     args = parse_args()
@@ -60,16 +61,26 @@ def main():
     npmis = []
     labels = None
     top_k = None
+    
     for rand in range(NSEEDS):
         if args.clustering_algo == "KMeans":
             labels, top_k  = KMeans_model(intersection, rand)
+            bins, top_k_words = sort(labels, top_k, words_index_intersect)
+
         elif args.clustering_algo == "GMM":
             # top_k are indexes of the vocabulary
             labels, top_k  = GMM_model(intersection, rand)
+            bins, top_k_words = sort(labels, top_k, words_index_intersect)
+
+        elif args.clustering_algo == 'from_file':
+
+            with open('bert_topics.txt', 'r') as f:
+                top_k_words = f.readlines()
+            top_k_words = [tw.strip().replace(',', '').split() for tw in top_k_words]
 
         # don't overload function name.
-        bins, top_k_words = sort(labels, top_k, words_index_intersect)
-        npmis.append(get_npmi(top_k_words, test_word_to_file, test_files_num))
+        print(top_k_words)
+        npmis.append(get_npmi2(top_k_words, test_word_to_file, test_files_num))
     print("NPMI mean:" + str(np.mean(npmis)))
 
     #print_bins(bins, "word2vec", "")
@@ -127,7 +138,8 @@ def create_id_dict(id2name):
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("--entities", type=str, required=True, choices=["word2vec", "fasttext", "KG"])
-    parser.add_argument("--clustering_algo", type=str, required=True, choices=["KMeans", "GMM"])
+    parser.add_argument("--clustering_algo", type=str, required=True, choices=["KMeans", "GMM",
+        'from_file'])
     parser.add_argument( "--entities_file", type=str, help="entity file")
     parser.add_argument('--id2name', type=Path, help="id2name file")
     args = parser.parse_args()
@@ -292,11 +304,61 @@ def create_vocab_and_files(stopwords, type):
     return word_to_file, len(files)
 
 
+def npmi_wpair(word1, word2, word_in_file, window_total):
+    eps = 10**(-12)
+    
+    w1_count = 0
+    w2_count = 0
+    combined_count = 0
+
+    if word1 in word_in_file and word2 in word_in_file:
+        combined_count = len(set(word_in_file[word1]) & set(word_in_file[word2]))
+        w1_count = len(word_in_file.get(word1, []))
+        w2_count = len(word_in_file.get(word2, []))
+
+    #if combined_count == 0:
+    #    if w1_count == 0 or w2_count == 0:
+    #        result = 0
+    #    else:
+    #        result = -1
+
+    #else:
+    result = np.log(((float(combined_count)*float(window_total)) + eps)/ \
+                (float(w1_count*w2_count)+eps))
+    result = result / (-1.0*np.log(float(combined_count)/(window_total) + eps))
+
+    return result
+
+def calc_topic_coherence(topic_words, word_in_file, files_num):
+    topic_assoc = []
+    for i in range(0, len(topic_words)-1):
+        w1 = topic_words[i]
+        for j in range(i+1, len(topic_words)):
+            w2 = topic_words[j]
+    #        if w1 != w2:
+            topic_assoc.append(npmi_wpair(w1, w2, word_in_file, files_num))
+    if len(topic_assoc)==0:
+        pdb.set_trace()
+    return float(sum(topic_assoc))/len(topic_assoc)
+
+def get_npmi2(top_k_bins, word_in_file, files_num):
+    ntopics = 20
+    npmi_scores = np.zeros(ntopics)
+
+    for k in range(ntopics):
+        npmi_score = calc_topic_coherence(top_k_bins[k], word_in_file, files_num)
+        print(np.around(npmi_score, 5), " ".join(top_k_bins[k]))
+        npmi_scores[k] = np.around(npmi_score, 5)
+
+    return np.mean(npmi_scores)
+
+
 
 def get_npmi(top_k_bins, vocab, files_num):
     e = 10**(-12)
     npmi = np.zeros(20)
-
+    
+    
     for i in range(20):
         word_pair_counts = 0
         for j in range(len(top_k_bins[i])):
@@ -319,6 +381,7 @@ def get_npmi(top_k_bins, vocab, files_num):
                 npmi[i] = npmi[i] + npmi_j_k
             
             npmi[i] = npmi[i]/word_pair_counts
+        print(np.around(npmi[i], 5), top_k_bins[i])
 
     print(np.mean(npmi))
     return(np.mean(npmi))
