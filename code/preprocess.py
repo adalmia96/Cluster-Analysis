8f073f65721d7542c22de2064c59d8edcc7a3262
adalmia96 <adalmia1@jhu.edu>
2020-04-15 23:30:02 -0400

    refectors fetch-20

--- code/preprocess.py
+++ code/preprocess.py
@@ -69,34 +69,6 @@ def create_vocab_and_files_20news(stopwords, type):
 
     train_data = fetch_20newsgroups(data_home='./data/', subset=type, remove=('headers', 'footers', 'quotes'))
     files = train_data['data'];
-    strip_punct = str.maketrans("", "", string.punctuation)
-    strip_digit = str.maketrans("", "", string.digits)
-
-    for file_num in range(0, len(files)):
-        words = files[file_num].lower().split()
-        #words = [w.strip() for w in words]
-        for word in words:
-            word = word.translate(strip_punct)
-            word = word.translate(strip_digit)
-            if word in stopwords:
-                continue
-
-            if word in word_to_file:
-                word_to_file[word].add(file_num)
-                word_to_file_mult[word].append(file_num)
-            else:
-                word_to_file[word]= set()
-                word_to_file_mult[word] = []
-
-                word_to_file[word].add(file_num)
-                word_to_file_mult[word].append(file_num)
-
-    for word in list(word_to_file):
-        if len(word_to_file[word]) < 5:
-            word_to_file.pop(word, None)
-            word_to_file_mult.pop(word, None)
-    print("Files:" + str(len(files)))
-    print("Vocab: " + str(len(word_to_file)))
     #doc_to_word = np.zeros
     return create_vocab(stopwords, files)
 
@@ -132,7 +104,7 @@ def create_vocab(stopwords, data):
                 word_to_file_mult[word].append(file_num)
 
     for word in list(word_to_file):
-        if len(word_to_file[word]) < 5:
+        if len(word_to_file[word]) <= 5:
             word_to_file.pop(word, None)
             word_to_file_mult.pop(word, None)
 
