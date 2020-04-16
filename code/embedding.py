6a220824623ea14083b1d33fb027effe7d3add42
adalmia96 <adalmia1@jhu.edu>
2020-04-16 18:46:07 -0400

    fixed semicolon error

--- code/embedding.py
+++ code/embedding.py
@@ -104,7 +104,7 @@ def create_entities_ft(model, train_word_to_file, doc_info):
     words = []
     for word in train_word_to_file:
         if doc_info:
-            for i in train_word_to_file[words]
+            for i in train_word_to_file[words]:
                 vocab_embeddings.append(model.get_word_vector(word))
                 words.append(word)
         else:
